defe3b9939eff9d6caef73f87c74dcc8f506f82c
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-04 15:45:20 +0300

    Modified car.py

--- lib/car.py
+++ lib/car.py
@@ -1,8 +1,22 @@
-from .vehicle import vehicle
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.mileage = 0
 
-class Car(vehicle);
-    def __init__(self, wheel_size, wheel_number):
-        super().__init__(wheel_size, wheel_number)
+    def description(self):
+        return f"{self.year} {self.brand} {self.model}"
 
-    def go(self):
-        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    def mileage_counter(self, miles):
+        self.mileage += miles
+
+
+class Car(Vehicle):
+    def __init__(self, brand, model, year, doors):
+        super().__init__(brand, model, year)
+        self.doors = doors
+
+    def description(self):
+        desc = super().description()
+        return f"{desc} with {self.doors} doors"
--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python3
-mport unittest
+import unittest
 from lib.car import Car
 
 class TestCar(unittest.TestCase):
     def test_car_instantiation(self):
         car = Car(16, 4)
         self.assertIsInstance(car, Car)
-    
-    def test_car_go(self)
+
+    def test_car_go(self):
         car = Car(16, 4)
         self.assertEqual(car.go(), "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!")
 
-
 if __name__ == '__main__':
     unittest.main()
-
